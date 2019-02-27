cc91787d7d162d2311e19d3fb1176165fbf57fe5
LudivineHay <ludivine.hay.13@gmail.com>
2019-02-27 10:55:58 +0100

    Finish homepage

--- src/System/routes.php
+++ src/System/routes.php
@@ -10,7 +10,6 @@ $twig = new \Twig\Environment($loader);
 $twig->addGlobal('router', $app->getContainer()->get('router'));
 $twig->addGlobal('navbar', [
   'home' => 'Home',
-  'champs' => 'Champions',
   'login' => 'Login',
   'about' => 'About',
 ]);
@@ -21,31 +20,30 @@ $app->get('/', function (Request $request, Response $response) {
     return $response->getBody()->write($twig->render('home.twig', $args));
 })->setName('home');
 
-$app->get('/champs', function (Request $request, Response $response, array $args) {
-    global $twig;
-    $args['pagename'] = 'Champions';
-
-    $PDO = new PDO('pgsql:host=localhost;dbname=test', 'root', 'root');
-    //$PDO = new PDO('pgsql:host=localhost;port=5432;dbname=test;user=root;password=root');
-    $champs = $PDO->query('SELECT champ_name, price_BE, price_RP, lore, main_position, sub_position FROM champions')->fetchAll(PDO::FETCH_ASSOC);
-    $args['champs'] = $champs;
-
-    // Render index view
-    return $response->getBody()->write($twig->render('champs.twig', $args));
-})->setName('champs');
-
 $app->get('/login', function (Request $request, Response $response, array $args) {
     global $twig;
     $args['pagename'] = 'Login';
     return $response->getBody()->write($twig->render('login.twig', $args));
 })->setName('login');
 
-
-$app->get('/about', function (Request $request, Response $response, array $args) {
+$app->get('/team', function (Request $request, Response $response, array $args) {
     global $twig;
-    $args['pagename'] = 'About';
-    return $response->getBody()->write($twig->render('about.twig', $args));
-})->setName('about');
+    $args['pagename'] = 'Team';
+    return $response->getBody()->write($twig->render('team.twig', $args));
+})->setName('team');
+
+$app->get('/articles', function (Request $request, Response $response, array $args) {
+    global $twig;
+    $args['pagename'] = 'Articles';
+    return $response->getBody()->write($twig->render('articles.twig', $args));
+})->setName('articles');
+
+$app->get('/signin', function (Request $request, Response $response, array $args) {
+    global $twig;
+    $args['pagename'] = 'SignIn';
+    return $response->getBody()->write($twig->render('signin.twig', $args));
+})->setName('signin');
+
 
 
 
